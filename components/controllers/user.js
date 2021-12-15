84ba7a7b14b2e25515ae694b8efcb4896fb04486
Jose Luis Pérez <duinweb@MacBook-Pro.home>
2021-12-15 13:58:21 +0100

    controlador usuarios completado y funcional

--- components/controllers/user.js
+++ components/controllers/user.js
@@ -3,8 +3,9 @@ const {User} = require('../models/');
 const userRouter = Router();
 
 
-// Todos los usuarios
 
+
+// Mostramos todos los usuarios
 
 userRouter.get('/', async (req, res) => { 
     User.find((error, success) => {
@@ -14,6 +15,10 @@ userRouter.get('/', async (req, res) => {
         res.json(success);
     }
 })})
+
+
+
+
 
 // Busqueda por ID
 
@@ -31,6 +36,61 @@ userRouter.get('/:id', async (req, res) => {
 })
 
 
+
+
+// Creamos un usuario
+
+userRouter.post('/create', async (req, res) => {
+    const body = req.body;
+    const user = new User(body);
+    user.save(( error, success ) => {
+        if (error) {
+            res.status(500).json({error: error.message});
+        } else {
+            res.status(201).json(success);
+        }
+    });
+})
+
+
+
+
+
+// Actualizamos un usuario mediante su ID
+
+userRouter.patch('/patch/:id', (req, res) => {
+    const body = req.body;
+    const id = req.params.id;
+
+    User.findByIdAndUpdate(id, body, {new: true}, ( error, success ) => {
+       if(error) {
+           res.status(500).json({error: error.message});
+       } else if (!success) {
+           res.status(404);
+       } else {
+           res.status(204).send();
+       }
+    })
+})
+
+
+
+
+// Borramos un usuario
+
+userRouter.delete('/delete/:id', (req,res) => {
+    const id = req.params.id;
+
+    User.findByIdAndDelete(id, {}, ( error, success ) => {
+        if(error) {
+            res.status(500).json({error: error.message});
+        } else if (!success) {
+            res.status(404);
+        } else {
+            res.status(205).send();
+        }
+    })
+})
 
 
 
